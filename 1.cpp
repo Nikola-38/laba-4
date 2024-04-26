c93bba87d19fa36ad90d7467ff00a365f34fd3ba
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-04-26 23:23:10 +0700

    Update 1.cpp
    W
    hat

--- 1.cpp
+++ 1.cpp
@@ -1,105 +1,104 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 #include <iomanip>
 
 using namespace std;
 
 // Общая функция, которую мы пытаемся решить
 double function(double x) {
-    return pow(x, 2) + log(x) - 6;
+    return pow(x - 6, 2) + log(x);
 }
 
 // Производная функции
 double derivative(double x) {
-    return 2 * x + 1 / x;
+    return 2 * (x - 6) + 1 / x;
 }
 
-// Функция для вывода таблицы итераций для метода Ньютона
-void PrintTable(int iteration, double xn, double xn_plus_1, double difference) {
-    cout << iteration << "\t" <<setw(10) << xn << "\t"<< setw(12) << xn_plus_1 << "\t"<< setw(14) << difference << endl;
+// Функция g(x) для метода простых итераций
+double g(double x) {
+    return exp((6 - sqrt(x * x)) / 2);
 }
 
-void PrintfTablHalfDivisionMethod(double LeftPoint, double RightPoint, int N)
-{
-    if (N == 1)
-    {
-        cout << "N";
-        cout << "\t     a ";
-        cout << "\t\t\t     b ";
-        cout << "\t\t\t     b-a ";
-        cout << endl;
-        cout << "----------------------------------------------------------------------------------";
-        cout << endl;
-    }
-    cout << N << " |";
-    cout <<"\t" << setw(10) << LeftPoint;
-    cout << "\t\t" << setw(10) << RightPoint;
-    cout << "\t\t" << setw(12)<< RightPoint - LeftPoint;
-    cout << endl;
-}
-
-
-// Функция для вывода таблицы итераций для метода простой итерации
-void PrintTableSimpleIterationMethod(int iteration, double xn, double xn_plus_1, double difference) {
-    cout << iteration << "\t" << setw(10) << xn << "\t" << setw(12) << xn_plus_1 << "\t" << setw(14) << difference << endl;
-}
-
-// Метод Ньютона
+// Метод Ньютона для поиска корня
 double newtonMethod(double x0, double epsilon) {
     double xn = x0;
-    double xn_plus_1 = xn - function(xn) / derivative(xn);
-    int iteration = 1;
+    double xn_plus_1;
+    int iteration = 1; // Инициализируем счетчик итераций с 1
 
-    cout << "Iteration\txn\t\txn+1\t\tdifference" << endl;
-    while (abs(xn_plus_1 - xn) > epsilon) {
-        cout << iteration << "\t" << setw(10) << xn << "\t" << setw(12) << xn_plus_1 << "\t" << setw(14) << abs(xn_plus_1 - xn) << endl;
-        xn = xn_plus_1;
-        xn_plus_1 = xn - function(xn) / derivative(xn);
-        iteration++;
-    }
-    return xn_plus_1;
-}
+    cout << "Iteration\txn\t\txn+1\t\tdifference" << std::endl;
 
-// Метод простых итераций
-double simpleIterationMethod(double x0, double epsilon) {
-    double xn = x0;
-    int iteration = 1;
-
-    cout << "Iteration\txn\t\txn+1\t\tdifference" << endl;
     do {
-        double xn_plus_1 = sqrt(6 - log(xn)); // Преобразуем уравнение x^2 + log(x) - 6 = 0 к виду x = sqrt(6 - log(x))
-        cout << iteration << "\t" << setw(10) << xn << "\t" << setw(10) << xn_plus_1 << "\t" << setw(14) << abs(xn_plus_1 - xn) << endl;
+        xn_plus_1 = xn - function(xn) / derivative(xn);
+        cout << iteration << setw(20) << xn << setw(20) << xn_plus_1 << setw(20) << abs(xn_plus_1 - xn) << endl;
         xn = xn_plus_1;
-        iteration++;
+        iteration++; // Инкрементируем счетчик итераций
     } while (abs(function(xn)) > epsilon);
 
     return xn;
 }
 
-double HalfDivisionMethod(double LeftPoint, double RightPoint,double epsilon)
-{
+// Метод простых итераций для поиска корня
+double simpleIterationMethod(double x0, double epsilon) {
+    double xn = x0;
+    double xn_plus_1;
+    int iteration = 0;
+    bool repeated = false;
+    vector<double> roots; // Вектор для хранения предыдущих корней
+
+    cout << "Iteration\txn\t\txn+1\t\tdifference" << std::endl;
+
+    do {
+        xn_plus_1 = g(xn);
+        cout << iteration << setw(15) << xn << setw(15) << xn_plus_1 << setw(15) << abs(xn_plus_1 - xn) << endl;
+
+        // Проверяем, не повторяется ли ответ
+        for (double root : roots) {
+            if (abs(xn_plus_1 - root) < epsilon) {
+                repeated = true;
+                break;
+            }
+        }
+
+        if (!repeated) {
+            roots.push_back(xn_plus_1); // Добавляем корень в вектор
+        } 
+        xn = xn_plus_1;
+        iteration++;
+    } while (abs(g(xn)) > epsilon && iteration < 100 && !repeated);
+
+    return xn;
+}
+
+// Метод половинного деления для поиска корня
+double HalfDivisionMethod(double a, double b, double epsilon) {
     int iteration = 1;
-    double midPoint = 0.0;
-    if (function(LeftPoint) * function(RightPoint) < 0)// проверка на разность знаков функции на концах отрезка
-    {
-        while (abs(RightPoint - LeftPoint) > epsilon)// пока интервал больше погрешности
-        {
-            midPoint = (RightPoint + LeftPoint) / 2;
-            PrintfTablHalfDivisionMethod(LeftPoint, RightPoint, iteration);
-            if (function(LeftPoint) * function(midPoint) < 0)  RightPoint = midPoint;// если функция имеет разные знаки, то правая точка середина отрезка
-            else LeftPoint = midPoint;
+    double midPoint;
+
+    cout << "N\ta\t\tb\t\tb-a" << std::endl;
+    cout << "---------------------------------" << std::endl;
+
+    if (function(a) * function(b) < 0) { // проверка на разность знаков функции на концах отрезка
+        while (abs(b - a) > epsilon) { // пока интервал больше погрешности
+            midPoint = (b + a) / 2.0;
+            cout << iteration << setw(10) << a << setw(14) << b << setw(14) << b - a << endl;
+            if (function(a) * function(midPoint) < 0) {
+                b = midPoint; // если функция имеет разные знаки, то правая точка становится серединой отрезка
+            } else {
+                a = midPoint; // иначе левая точка становится серединой отрезка
+            }
             iteration++;
         }
-    } 
-    else {
-        cout << "The interval is not selected correctly. There is no solution in the segment" << endl;
+        return midPoint; // возвращаем найденный корень
+    } else {
+        cout << "Интервал выбран неправильно. В данном сегменте нет решения" << endl;
+        return NAN;
     }
-    return midPoint;
 }
 
 int main() {
-    double a = 1; // Начало интервала
-    double b = 5; // Конец интервала
+    double a = 0.0; // Начало интервала
+    double b = 5.0; // Конец интервала
     double epsilon = 0.0001; // Погрешность
     system("chcp 65001"); // Команда для поддержки русского языка в консоли
 
