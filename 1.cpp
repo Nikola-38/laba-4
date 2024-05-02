8a32b31299c8b2c283b7dd032905f1259305eb81
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-05-03 01:17:53 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -15,59 +15,55 @@ double derivative(double x) {
     return 2 * (x - 6) + 1 / x;
 }
 
+
+// Вспомогательная функция для метода простых итераций
 // Функция g(x) для метода простых итераций
 double g(double x) {
     return exp((6 - sqrt(x * x)) / 2);
 }
 
-// Метод Ньютона для поиска корня
-double newtonMethod(double x0, double epsilon) {
-    double xn = x0;
-    double xn_plus_1;
-    int iteration = 1; // Инициализируем счетчик итераций с 1
-
-    cout << "Iteration\txn\t\txn+1\t\tdifference" << std::endl;
-
-    do {
-        xn_plus_1 = xn - function(xn) / derivative(xn);
-        cout << iteration << setw(20) << xn << setw(20) << xn_plus_1 << setw(20) << abs(xn_plus_1 - xn) << endl;
-        xn = xn_plus_1;
-        iteration++; // Инкрементируем счетчик итераций
-    } while (abs(function(xn)) > epsilon);
-
-    return xn;
-}
-
-// Метод простых итераций для поиска корня
 double simpleIterationMethod(double x0, double epsilon) {
     double xn = x0;
     double xn_plus_1;
     int iteration = 0;
-    bool repeated = false;
-    vector<double> roots; // Вектор для хранения предыдущих корней
 
-    cout << "Iteration\txn\t\txn+1\t\tdifference" << std::endl;
+    cout << "Iteration\txn\t\txn+1\t\tdifference" << endl;
 
     do {
         xn_plus_1 = g(xn);
-        cout << iteration << setw(15) << xn << setw(15) << xn_plus_1 << setw(15) << abs(xn_plus_1 - xn) << endl;
+        cout << iteration << "\t" << setprecision(10) << xn << "\t" << setprecision(10) << xn_plus_1 << "\t" << fixed << setprecision(6) << abs(xn_plus_1 - xn) << endl;
 
-        // Проверяем, не повторяется ли ответ
-        for (double root : roots) {
-            if (abs(xn_plus_1 - root) < epsilon) {
-                repeated = true;
-                break;
-            }
+        if (abs(xn_plus_1 - xn) < epsilon) {
+            return xn_plus_1; // Возвращаем найденный корень
         }
 
-        if (!repeated) {
-            roots.push_back(xn_plus_1); // Добавляем корень в вектор
-        } 
         xn = xn_plus_1;
         iteration++;
-    } while (abs(g(xn)) > epsilon && iteration < 100 && !repeated);
+    } while (iteration < 50);
+    return xn_plus_1; // Возвращаем найденный корень
 
-    return xn;
+}
+
+// Метод отделения корней
+pair<double, double> separate_roots(double a, double b, double epsilon) {
+    if (function(a) * function(b) >= 0) {
+        cout << "На концах отрезка [a, b] функция имеет одинаковые знаки, метод отделения корней не может быть применен." << endl;
+        return make_pair(NAN, NAN);
+    }
+
+    double c = a;
+    while ((b - a) >= epsilon) {
+        c = (a + b) / 2;
+        if (function(c) == 0.0) {
+            break;
+        } else if (function(c) * function(a) < 0) {
+            b = c;
+        } else {
+            a = c;
+        }
+    }
+
+    return make_pair(a, b);
 }
 
 // Метод половинного деления для поиска корня
@@ -78,37 +74,42 @@ double HalfDivisionMethod(double a, double b, double epsilon) {
     cout << "N\ta\t\tb\t\tb-a" << std::endl;
     cout << "---------------------------------" << std::endl;
 
-    if (function(a) * function(b) < 0) { // проверка на разность знаков функции на концах отрезка
-        while (abs(b - a) > epsilon) { // пока интервал больше погрешности
-            midPoint = (b + a) / 2.0;
-            cout << iteration << setw(10) << a << setw(14) << b << setw(14) << b - a << endl;
-            if (function(a) * function(midPoint) < 0) {
-                b = midPoint; // если функция имеет разные знаки, то правая точка становится серединой отрезка
-            } else {
-                a = midPoint; // иначе левая точка становится серединой отрезка
-            }
-            iteration++;
-        }
-        return midPoint; // возвращаем найденный корень
-    } else {
-        cout << "Интервал выбран неправильно. В данном сегменте нет решения" << endl;
+    if (function(a) * function(b) > 0) {
+        cout << "Интервал выбран неправильно. В данном сегменте нет решения." << endl;
         return NAN;
     }
+
+    while (abs(b - a) > epsilon) {
+        midPoint = (b + a) / 2.0;
+        cout << iteration << setw(10) << a << setw(14) << b << setw(14) << b - a << endl;
+        if (function(a) * function(midPoint) < 0) {
+            b = midPoint;
+        } else {
+            a = midPoint;
+        }
+        iteration++;
+    }
+    return midPoint;
 }
 
 int main() {
     double a = 0.0; // Начало интервала
-    double b = 5.0; // Конец интервала
+    double b = 5; // Конец интервала
     double epsilon = 0.0001; // Погрешность
+    double X0 =  0.5; 
     system("chcp 65001"); // Команда для поддержки русского языка в консоли
 
-    cout << "Корни, найденные методом Ньютона:" << endl;
-    double rootNewton = newtonMethod((a + b) / 2, epsilon);
-    cout << "Root: " << rootNewton << endl;
-
     cout << "\nКорни, найденные простым итерационным методом:" << endl;
-    double rootSimpleIteration = simpleIterationMethod((a + b) / 2, epsilon);
-    cout << "Root: " << rootSimpleIteration << endl;
+    double rootSimpleIteration = simpleIterationMethod(X0, epsilon);
+    if (!isnan(rootSimpleIteration)) {
+        cout << "Root: " << setprecision(6) << rootSimpleIteration << endl;
+    }
+    
+    cout << "Интервалы, содержащие корни, найденные методом отделения корней:" << endl;
+    pair<double, double> rootInterval = separate_roots(a, b, epsilon);
+    if (!isnan(rootInterval.first) && !isnan(rootInterval.second)) {
+        cout << "Интервал: [" << rootInterval.first << ", " << rootInterval.second << "]" << endl;
+    };
 
     cout << "\nКорни, найденные методом половинного деления:" << endl;
     double rootHalfDivision = HalfDivisionMethod(a, b, epsilon);
